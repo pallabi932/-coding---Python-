3b1ee7e30a9187c8a9eb2438d20c6f924f189522
pallabi932 <pg1647004@gmail.com>
2026-02-10 12:58:03 +0530

    Split  joint.py

---
+++ Split  joint.py
@@ -0,0 +1,9 @@
+a= "my name is pallabi"
+b=a.split ()print (b)C="
+".join(b)
+print(c)
+Window
+Help
+sentence="i am pallabi"words=sentence, split ()words.reverse()
+reversed sentence="".join(words)
+print(reversed sentence)

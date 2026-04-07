f3cc7c1f2fefbca43ad94c929b04a0f2e8538388
pallabi932 <pg1647004@gmail.com>
2026-04-07 12:36:36 +0530

    class.py

---
+++ class.py
@@ -0,0 +1,17 @@
+class Node:
+def__init_(self, data)：
+self.data=data
+self.left=None
+self.right=None
+self.parent=None
+root=None
+а=[1, 10, 11, 8, 6, 12, 14, 7]
+# for val in a:
+print(val)
+print(root)
+root=Node(a[9])
+print(root)
+ print(root.data)
+ b=Node(a[1])
+print(b)
+print(b.data)

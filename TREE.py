4c406e29a8075a324ef8b0368673cd68fdd40d94
pallabi932 <pg1647004@gmail.com>
2026-04-07 13:05:30 +0530

    TREE.py

--- TREE.py
+++ TREE.py
@@ -16,3 +16,38 @@ return root
 if root:
 inorder(root.left)
 print(root.val, end=' ')
+inorder (root.right)
+ def preorder (root):
+if root:
+ print(root.val,end=' ')
+preorder (root.left)
+def postorder (root):
+preorder (root.right)
+if root:
+postorder (root.left)
+postorder (root.right)
+print(root.val,end=' ')
+ #Example usage:
+ r=node (50)
+print(r.val)
+inorder(root.right)
+def preorder(root):
+if root:
+print(root.val, end='')
+preorder(root.left)
+preorder(root.pight)
+def postorder(root)：
+if root:
+postorder(root.left)
+postorder(root.right)
+print(root.val, end='')
+ #Example usage:
+r=node(50)
+ print(r.val)
+p=insert(r, 30)+ r=insert(r, 20)+ print(r.val)+ r=insert(r, 40)+ r=insert(r, 70)+ r=insert(r, 60)+ print(r.val)+ r=insert(r, 80)
+ print("Inorder traversal:")
+inorder(r)
+print("InPreorder traversal:")
+preorder(r)
+print("(nPostorder traversal:")
+ postorder(r)

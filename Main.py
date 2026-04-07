5dc516d9e984a6dfbc0053ea814a77fbd0d66b9f
pallabi932 <pg1647004@gmail.com>
2026-04-07 12:55:00 +0530

    Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+ data1={
+"Name": ['riya', 'jiya', 'liza'], 
+ "Roll": ['12', '23', '21'],
+"Marks": ['20', '12', '19']
+}
+df1=pd.DataFrame(data1)
+data2={
+"Name": ['riya', 'jiya', 'liza'],
+ "Roll": ['12', '23', '21'],
+"Attendance": ['70%', '80%',  '90%']
+}
+df2=pd.DataFrame(data2) df3=pd.merge(df1,df2.on='Roll',how='outer')
+ print(df3)

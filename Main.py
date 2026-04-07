936a0f531bbc14a0539c2341501a38fdc426765f
pallabi932 <pg1647004@gmail.com>
2026-04-07 12:56:33 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -10,5 +10,6 @@ data2={
  "Roll": ['12', '23', '21'],
 "Attendance": ['70%', '80%',  '90%']
 }
-df2=pd.DataFrame(data2) df3=pd.merge(df1,df2.on='Roll',how='outer')
+df2=pd.DataFrame(data2) 
+df3=pd.merge(df1,df2.on='Roll',how='outer')
  print(df3)
